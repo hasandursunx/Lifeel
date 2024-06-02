471c61bf4b0f47ad9ed6e5b0b8a1de4db0ca3bd9
Hasan Dursun <hasandursunx@gmail.com>
2024-06-03 00:54:56 +0300

    firebase deleted

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express"
 const app = express()
 const port = 3000
 
+//static files middleware
+app.use(express.static('public'))
+
 app.get("/",(req,res)=>{
     res.send("INDEX SAYFASI 2")
 })
